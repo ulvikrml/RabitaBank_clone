f71ec1605ad5baf44c43be929c33a294e7663bf9
ulvikrml <ulvikrml@gmail.com>
2023-04-21 20:15:18 +0400

    v1.3.4 'currency convertor fixing'

--- src/components/Calculator/Conversion.js
+++ src/components/Calculator/Conversion.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import CurrencyRow from './CurrencyRow'
 
-const API_KEY = 'H6JuCTlxDYhwtIfzHVArKAArWqAGSyGz'
-
 const Conversion = () => {
   // const [result, setResult] = useState('null')
   const [fromCurrency, setFromCurrency] = useState('EUR')
@@ -11,29 +9,37 @@ const Conversion = () => {
   const [amountTo, setAmountTo] = useState(1.87)
   const [exchangeRate, setExchangeRate] = useState(1)
 
-  // useEffect(() => {
-  //   fetch(`https://api.apilayer.com/fixer/convert?to=${toCurrency}&from=${fromCurrency}&amount=1&apikey=H6JuCTlxDYhwtIfzHVArKAArWqAGSyGz`)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log(data);
-  //       setExchangeRate(data.info.rate)
-  //     })
-  //   console.log('useEffect');
-  // }, [fromCurrency, toCurrency])
+  useEffect(() => {
+    const fetchHandler = () =>{
+      fetch(`https://api.exchangerate.host/latest?base=${fromCurrency}}&symbols=${toCurrency}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.rates[toCurrency]);
+        setExchangeRate(data.rates[toCurrency])
+      })
+    }
+    fetchHandler()
+      console.log('useEffect');
+      // console.log(exchangeRate);
+      // console.log(fromCurrency);
+  }, [fromCurrency, toCurrency])
 
   const fromAmountHandler = (e) => {
-    setAmountFrom(+(e.target.value))
-    console.log(amountFrom);
-    console.log(amountTo);
-    setAmountTo(+(e.target.value) * exchangeRate)
-    console.log(amountTo);
+    let value = +(e.target.value)
+    setAmountFrom(value)
+    setAmountTo((value * exchangeRate).toFixed(2))
   }
   const toAmountHandler = (e) => {
-    setAmountTo(+(e.target.value))
-    setAmountTo(amountTo * exchangeRate)
+    let value = +(e.target.value)
+    setAmountTo(value)
+    setAmountFrom((value / exchangeRate).toFixed(2))
   }
-  // console.log(amountFrom);
-  // console.log(amountTo);
+  const fromCurrencyHandler = (e) =>{
+    setFromCurrency(e.target.value)
+    setAmountTo((amountFrom / exchangeRate).toFixed(2))
+  }
+  console.log(fromCurrency);
+  console.log(toCurrency);
   return (
     <div>
       <div className='flex mb-2 items-center'>
@@ -41,7 +47,7 @@ const Conversion = () => {
       </div>
       <div className='flex'>
         <form>
-          <CurrencyRow selectedCurrency={fromCurrency} changeCurrency={(e) => setFromCurrency(e.target.value)} amount={amountFrom} changeAmount={fromAmountHandler} />
+          <CurrencyRow selectedCurrency={fromCurrency} changeCurrency={(e) => fromCurrencyHandler(e)} amount={amountFrom} changeAmount={fromAmountHandler} />
           =
           <CurrencyRow selectedCurrency={toCurrency} changeCurrency={(e) => setToCurrency(e.target.value)} amount={amountTo} changeAmount={toAmountHandler} />
         </form>
